d910af334984248019c0071908deffa0053e5404
YJH <yooy1313@naver.com>
2023-09-26 17:29:26 +0900

    조건문 학습

---
+++ Mycondition.java
@@ -0,0 +1,17 @@
+package com.mystudy.run;
+
+public class Mycondition {
+    public static void main(String[] args) {
+        int a = 30;
+        String pan = " ";
+        if (a >= 150) {
+            pan = "상";
+        } else if (a >= 50) {
+            pan = "중";
+        } else {
+            pan = "하";
+        } System.out.println(pan);
+    }
+}
+
+
