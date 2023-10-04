5cec7e592140180a7cdd9cfbb320a55f07e7caae
YJH <yooy1313@naver.com>
2023-10-04 17:05:52 +0900

    쉬프트 연산자 학습

---
+++ BinaryShift.java
@@ -0,0 +1,12 @@
+package com.mystudy.binary;
+
+public class BinaryShift {
+    public static void main(String[] args) {
+
+        int i = 13;
+        System.out.println("i<<1 : " + (i<<1));
+        System.out.println("i<<2 : " + (i<<2));
+        System.out.println("i>>1 : " + (i>>1));
+        System.out.println("i>>2 : " + (i>>2));
+    }
+}
