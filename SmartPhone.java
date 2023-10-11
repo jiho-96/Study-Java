5b7fa64d3d5068aa8798df9ab5e16ae746e93423
YJH <yooy1313@naver.com>
2023-10-11 17:29:08 +0900

    클래스 상속

---
+++ SmartPhone.java
@@ -0,0 +1,10 @@
+package com.mystudy.inher;
+
+public class SmartPhone extends Phone {
+
+    public SmartPhone(String model, String color) {
+        super(model, color);
+        System.out.println("SmartPhone(String model, String color) 생성자 실행됨");
+
+    }
+}
