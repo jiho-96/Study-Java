d41b79571ec5a25cea14020611f34c8f1ac0a2b2
YJH <yooy1313@naver.com>
2023-10-13 16:48:09 +0900

    예외 떠넘기기 학습

---
+++ ThrowsExample2.java
@@ -0,0 +1,11 @@
+package com.mystudy.expt;
+
+public class ThrowsExample2 {
+    public static void main(String[] args) throws Exception{
+        findClass();
+    }
+
+    public  static void findClass() throws ClassNotFoundException {
+        Class.forName("java.lang.String2");
+    }
+}
