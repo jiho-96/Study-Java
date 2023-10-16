8857a7ab293ecf7b886a064c416505186427e96f
YJH <yooy1313@naver.com>
2023-10-16 17:11:49 +0900

    정규표현

---
+++ RegexExam3.java
@@ -0,0 +1,22 @@
+package com.mystudy.re;
+
+import java.util.regex.Pattern;
+
+public class RegexExam3 {
+    public static void main(String[] args) {
+        String name = "홍길동";
+        String tel = "010-1234-5678";
+        String email = "test@naver.com";
+
+        //유효성 검사
+        boolean name_check = Pattern.matches("^[가-힣]*$", name);
+        boolean tel_check = Pattern.matches("^01(?:0|1|[6-9])-(?:\\d{4})-\\d{4}$", tel);
+        boolean email_check = Pattern.matches("\\w+@\\w+\\.\\w+(\\.\\w+)?", email);
+
+        //출력
+        System.out.println("이름 : " + name_check);
+        System.out.println("전화번호 : " + tel_check);
+        System.out.println("이메일 : " + email_check);
+
+    }
+}
